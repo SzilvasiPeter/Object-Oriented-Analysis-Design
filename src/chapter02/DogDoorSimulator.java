dea4b73958f09ad3f3b675a8856cf3d77ddfefe4
SzilvasiPeter <peti.szilvasi95@gmail.com>
2020-01-27 20:28:17 +0100

    Automatic closing remote control

--- src/chapter02/DogDoorSimulator.java
+++ src/chapter02/DogDoorSimulator.java
@@ -8,10 +8,16 @@ public class DogDoorSimulator {
         System.out.println("Fido barks to go outside...");
         remote.pressButton();
         System.out.println("Fido has gone outside...");
-        remote.pressButton();
         System.out.println("Fido's all done...");
+
+        try{
+            Thread.sleep(10000);
+        } catch (InterruptedException e){ }
+        System.out.println("... but he is stuck outside");
+        System.out.println("Fido scratches at the door");
+        System.out.println("Fido starts barking");
+        System.out.println("... so Todd grabs the remote control");
         remote.pressButton();
         System.out.println("Fido's back inside...");
-        remote.pressButton();
     }
 }
--- src/chapter02/Remote.java
+++ src/chapter02/Remote.java
@@ -1,5 +1,8 @@
 package chapter02;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Remote {
     private DogDoor door;
 
@@ -13,6 +16,15 @@ public class Remote {
             door.close();
         } else {
             door.open();
+
+            final Timer timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    door.close();
+                    timer.cancel();
+                }
+            }, 5000);
         }
     }
 }
