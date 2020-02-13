063ab110e3d9c9e8613c2f71fa9616227895362f
SzilvasiPeter <peti.szilvasi95@gmail.com>
2020-02-13 19:18:38 +0100

    Highlight the commonality between the units in the Unit class

--- src/chapter07/Unit.java
+++ src/chapter07/Unit.java
@@ -1,7 +1,29 @@
 package chapter07;
 
+import java.util.Map;
+
 public class Unit {
 
+    private String type;
+    private Map properties;
+
     public Unit() {
+
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Map getProperties() {
+        return properties;
+    }
+
+    public void setProperties(Map properties) {
+        this.properties = properties;
     }
 }
